7e0bf0a10a6cbcdc5a0728fef72d9dd492d56762
marcosrafaellsousa <marcosrafaellsousa@gmail.com>
2019-05-29 19:40:27 -0300

    resolvendo problema da classe localizacao

---
+++ Codigo/src/main/java/com/github/saulocalixto/culty/cultyserver/model/Localizacao.java
@@ -0,0 +1,42 @@
+package com.github.saulocalixto.culty.cultyserver.model;
+
+public class Localizacao {
+
+    private String cidade;
+    private String estado;
+    private String pais;
+
+    public Localizacao() {
+    }
+
+    public Localizacao(String cidade, String estado, String pais) {
+        this.cidade = cidade;
+        this.estado = estado;
+        this.pais = pais;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+}
+/**/
