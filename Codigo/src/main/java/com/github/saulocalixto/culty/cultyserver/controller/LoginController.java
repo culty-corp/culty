d50ec0551d12285a3f2514ec0bc66819749b2b3d
saulocalixto <sauloacalixto@gmail.com>
2019-06-30 13:13:43 -0300

    Mudanças em controller de login

--- Codigo/src/main/java/com/github/saulocalixto/culty/cultyserver/controller/LoginController.java
+++ Codigo/src/main/java/com/github/saulocalixto/culty/cultyserver/controller/LoginController.java
@@ -1,18 +1,11 @@
 package com.github.saulocalixto.culty.cultyserver.controller;
 
 import com.github.saulocalixto.culty.cultyserver.dto.DTOSessao;
-import com.github.saulocalixto.culty.cultyserver.model.Sessao;
 import com.github.saulocalixto.culty.cultyserver.servico.ServicoSessao;
-import com.github.saulocalixto.culty.cultyserver.servico.ServicoUsuario;
-import com.github.saulocalixto.culty.cultyserver.servico.contrato.IServicoPadrao;
 import com.github.saulocalixto.culty.cultyserver.servico.contrato.IServicoSessao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
-import java.util.HashMap;
 
 @CrossOrigin
 @RequestMapping("/login")
@@ -21,9 +14,6 @@ public class LoginController {
 
     @Autowired
     private ServicoSessao servico;
-
-    @Autowired
-    private ServicoUsuario servicoUsuario;
 
     @Autowired
     protected IServicoSessao Servico() {
